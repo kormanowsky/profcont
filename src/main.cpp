012bb89c236e4dd24cb761aee1c33118516b02cb
Михаил Кормановский <kormanowsky@gmail.com>
2022-10-19 13:06:29 +0300

    add: argparse for interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <memory>
+#include <vector>
+#include <string>
 
-#include "dlfcn.h"
+#include "dependencies/argparse/argparse.hpp"
 
 #include "extension/base_extension.hpp"
 #include "extension/extension_loader.hpp"
@@ -10,20 +12,46 @@
 #include "rule/no_vowels_rule.hpp"
 #include "transformer/transformer.hpp"
 
-int main()
+class Solution
 {
+public:
+    Solution(int argc, char **argv)
+    {}
+
+    void run()
+    {}
+};
+
+int main(int argc, char **argv)
+{
+    argparse::ArgumentParser parser("profcont");
+    parser.add_description("Un Profesor Contento!\n"
+                           "Transforms text files and performs useful checks to help students pass their reports quickly");
+    parser.add_argument("input_file").help("input file").required();
+    parser.add_argument("-o", "--output").help("output file (if needed)").default_value("/dev/null");
+    parser.add_argument("-e", "--extension").help("one or more extensions (if needed)").append();
+    try
+    {
+        parser.parse_args(argc, argv);
+    } catch (const std::runtime_error &err)
+    {
+        std::cerr << parser << std::endl;
+        std::cerr << err.what() << std::endl;
+        return 1;
+    }
+    // а пока так
     ExtensionLoader loader;
     std::shared_ptr<BaseRule> rule = std::make_shared<NoVowelsRule>();
-    std::string extension;
-    std::cin >> extension;
-    if (extension == "ext_aa")
+    auto extensions = parser.get<std::vector<std::string>>("--extension");
+    std::string our_extension = "ext_aa";
+    auto occurrence = std::find(extensions.begin(), extensions.end(), our_extension);
+    if (occurrence != extensions.end())
     {
-        auto ext = loader.load_extension(extension);
+        auto ext = loader.load_extension(*occurrence);
         rule = ext->get_rule();
     }
-    std::string filename;
-    std::string out_filename;
-    std::cin >> filename >> out_filename;
+    std::string filename = parser.get("input_file");
+    std::string out_filename = parser.get("--output");
     std::shared_ptr<BaseSource> src = std::make_shared<FileSource>(filename);
     std::shared_ptr<BaseTarget> tgt = std::make_shared<FileTarget>(out_filename);
 
